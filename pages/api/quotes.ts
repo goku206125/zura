962eaeaa060b2922dd04b8ff09ee7a64e6a58001
goku206125 <75624413+goku206125@users.noreply.github.com>
2025-07-06 03:26:00 +0200

    fixed: syntax error in the API

--- pages/api/quotes.ts
+++ pages/api/quotes.ts
@@ -1,10 +1,7 @@
-import { PrismaClient }  from "@prisma/client"; 
-const prisma = new PrismaClient():
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default async function handler(req:res) {
-  
- const quotes = await prisma.quote.findMany();
- resizeBy.status(200).json(quotes);
- 
+export default async function handler(req, res) {
+  const quotes = await prisma.quote.findMany();
+  res.status(200).json(quotes);
 }
-
